4a73bd9d1c9019386cdbec6ae201751927ce13dc
Aamir <57533908+aamir-commit@users.noreply.github.com>
2026-01-10 00:45:12 +0530

    Update Test4Application.java
    changes made to leearn merge conflict

--- src/main/java/com/test4/Test4Application.java
+++ src/main/java/com/test4/Test4Application.java
@@ -7,7 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test4Application {
 
 	public static void main(String[] args) {
+		        string name = "mike"
                 int x = 100;
+		        boolean isPresent=false;
+		        int y = 200;
+		        char x = "a";
+		        int y =200;
+		         double z =10.3
+			    boolean isPresent=false;
+		        float m = 10.3f;
 		SpringApplication.run(Test4Application.class, args);
 	}
 
